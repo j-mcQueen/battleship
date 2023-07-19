85c93d33d9fd1c7651d412b77ede6e3e61668deb
jack mcqueen <jdmcqueen44@gmail.com>
2023-07-19 13:22:38 -0400

    Test ship placement & sunk fns

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,7 @@
-import { gameboard } from "../components/Gameboard";
-import { destroyer } from "../components/Ship";
+import { Ship } from "../components/Ship";
+import { Gameboard } from "../components/Gameboard";
+
+const gameboard = Gameboard();
 
 const placeShip = (coords, ship, board) => {
   // place a horizontal ship
@@ -14,6 +16,8 @@ const placeShip = (coords, ship, board) => {
 };
 
 test("Expects a horizontal destroyer placed at coords [[0,0], [1,0]] to produce a board that matches the expected board", () => {
+  const destroyer = Ship("XS");
+
   let freshBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -53,6 +57,8 @@ test("Expects a horizontal destroyer placed at coords [[0,0], [1,0]] to produce 
 });
 
 test("Expects a horizontal destroyer placed at random coords [[3,4], [4,4]] to produce a board that matches the expected board", () => {
+  const destroyer = Ship("XS");
+
   let freshBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -93,6 +99,8 @@ test("Expects a horizontal destroyer placed at random coords [[3,4], [4,4]] to p
 
 test.skip("Expects a vertical destroyer placed at coords [[0,0], [0,1] to produce a board that matches the expected board", () => {
   // to get working, go into Ship.js and change the axis value to 1
+  const destroyer = Ship("XS");
+
   let freshBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -133,6 +141,8 @@ test.skip("Expects a vertical destroyer placed at coords [[0,0], [0,1] to produc
 
 test.skip("Expects a vertical destroyer placed at random coords [[3,4], [3,5] to produce a board that matches the expected board", () => {
   // to get working, go into Ship.js and change the axis value to 1
+  const destroyer = Ship("XS");
+
   let freshBoard = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -172,6 +182,8 @@ test.skip("Expects a vertical destroyer placed at random coords [[3,4], [3,5] to
 });
 
 test("When hit, expect all destroyer coords to return the same number of strikes", () => {
+  const destroyer = Ship("XS");
+
   let newestBoard = [
     [destroyer, destroyer, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -213,41 +225,167 @@ test("When hit, expect all destroyer coords to return the same number of strikes
   expect(newestBoard[3][4]).toEqual(0);
 });
 
-// test("Expects a hit ship to return the number of strikes on the ship", () => {
-//   // I want to test if both destroyers have the same strikes value after the hit function is called on one
-//   let board = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, destroyer, destroyer, 0, 0, 0, 0, 0],
-//     [0, 0, 0, , 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   ];
+test("After initialization, expect board to contain placed ships", () => {
+  const destroyer = Ship("XS");
+  const submarine = Ship("S");
+  const battleship = Ship("M");
+  const carrier = Ship("L");
 
-//   // gameboard.placeShip(
-//   //   [
-//   //     [3, 6],
-//   //     [4, 6],
-//   //   ],
-//   //   destroyer
-//   // );
+  let freshBoard = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
-//   expect(gameboard.receiveAttack([0, 1])).toEqual(board);
-// });
+  let expectedBoard1 = [
+    [destroyer, destroyer, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
 
-// test("Expects the provided coordinates in the board to reveal a ship object", () => {
-//   // create a variable which holds the board with a ships at specified positions
-//   // check if the value inside this board at the provided coordinates is the same as what is returned from receiveAttack
-//   let board = gameboard.placeShip(
-//     [
-//       [3, 6],
-//       [4, 6],
-//     ],
-//     destroyer
-//   );
-//   expect(gameboard.receiveAttack([3, 6])).toEqual(board[5][2]);
-// });
+  let expectedBoard2 = [
+    [destroyer, destroyer, 0, 0, 0, 0, 0, 0, 0, 0],
+    [submarine, submarine, submarine, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  let expectedBoard3 = [
+    [destroyer, destroyer, 0, 0, 0, 0, 0, 0, 0, 0],
+    [submarine, submarine, submarine, 0, 0, 0, 0, 0, 0, 0],
+    [battleship, battleship, battleship, battleship, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  let expectedBoard4 = [
+    [destroyer, destroyer, 0, 0, 0, 0, 0, 0, 0, 0],
+    [submarine, submarine, submarine, 0, 0, 0, 0, 0, 0, 0],
+    [battleship, battleship, battleship, battleship, 0, 0, 0, 0, 0, 0],
+    [carrier, carrier, carrier, carrier, carrier, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  expect(
+    placeShip(
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      destroyer,
+      freshBoard
+    )
+  ).toEqual(expectedBoard1);
+
+  expect(
+    placeShip(
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      submarine,
+      expectedBoard1
+    )
+  ).toEqual(expectedBoard2);
+
+  expect(
+    placeShip(
+      [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+      ],
+      battleship,
+      expectedBoard2
+    )
+  ).toEqual(expectedBoard3);
+
+  expect(
+    placeShip(
+      [
+        [0, 3],
+        [1, 3],
+        [2, 3],
+        [3, 3],
+        [4, 3],
+      ],
+      carrier,
+      expectedBoard3
+    )
+  ).toEqual(expectedBoard4);
+});
+
+test("Expect a gameboard to initially contain ships which are all afloat", () => {
+  const destroyer = Ship("XS");
+  const submarine = Ship("S");
+  const battleship = Ship("M");
+  const carrier = Ship("L");
+  const ships = [destroyer, submarine, battleship, carrier];
+
+  expect(gameboard.allSunk(ships)).toEqual(false);
+});
+
+test("Expect a gameboard to contain ships which are all sunk", () => {
+  const destroyer = Ship("XS");
+  const submarine = Ship("S");
+  const battleship = Ship("M");
+  const carrier = Ship("L");
+  const ships = [destroyer, submarine, battleship, carrier];
+
+  for (let i = 0; i < ships.length; i++) {
+    for (let j = 0; j < ships[i].length(); j++) {
+      ships[i].hit();
+    }
+  }
+
+  expect(gameboard.allSunk(ships)).toEqual(true);
+});
+
+test("Expect a gameboard to contain ships which are mostly sunk", () => {
+  const destroyer = Ship("XS");
+  const submarine = Ship("S");
+  const battleship = Ship("M");
+  const carrier = Ship("L");
+  const ships = [destroyer, submarine, battleship, carrier];
+
+  for (let i = 0; i < ships.length - 1; i++) {
+    // loops will end before carrier is hit
+    for (let j = 0; j < ships[i].length(); j++) {
+      ships[i].hit();
+    }
+  }
+
+  expect(gameboard.allSunk(ships)).toEqual(false);
+});
